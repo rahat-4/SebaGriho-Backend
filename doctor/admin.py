202272050b63e248a6f6dbb744f6bc69351f8fb3
rahat-4 <mainul.rahat4@gmail.com>
2024-06-17 13:49:00 +0600

    Change folder structure

--- doctor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Achievement, Degree, Department, Doctor
-
-admin.site.register(Achievement)
-admin.site.register(Degree)
-admin.site.register(Department)
-admin.site.register(Doctor)
--- patient/choices.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class PatientStatus(models.TextChoices):
-    DRAFT = "DRAFT", "Draft"
-    ACTIVE = "ACTIVE", "Active"
-    PAUSED = "PAUSED", "Paused"
-    REMOVED = "REMOVED", "Removed"
